ef5f8a2c71e1e56a24b4e262e414baaa72991efa
sunnysunnysun1 <sunnysunnysun1@gmail.com>
2025-06-27 17:12:51 +0530

    Update vcf_visualizer.py

--- vcf_visualizer.py
+++ vcf_visualizer.py
@@ -1,4 +1,11 @@
 import streamlit as st
+
+st.set_page_config(page_title="Your App", layout="wide")
+st.markdown(
+    "<style>body { margin: 0; }</style>", 
+    unsafe_allow_html=True
+)
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
